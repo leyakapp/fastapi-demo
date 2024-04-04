1b720fa5cbe5b6ca1f4b5994db3c98374b19fc52
Leya Kappukattil <mth8yq@virginia.edu>
2024-04-04 16:37:58 +0000

    edited  main

--- app/main.py
+++ app/main.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 
+import os
+import MySQLdb
+from fastapi.staticfiles import StaticFiles
 from fastapi import FastAPI
 from typing import Optional
 from pydantic import BaseModel
@@ -10,8 +13,12 @@ import MySQLdb
 from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
-app.mount("/static", StaticFiles(directory="static", html = True), name="static")
+app.mount("/static", StaticFiles(directory="static", html=True), name="static")
 
+DBHOST = os.environ.get('DBHOST')
+DBUSER = os.environ.get('DBUSER')
+DBPASS = os.environ.get('DBPASS')
+DB = "mth8yq"  # replace with your UVA computing ID / database name
 # db config stuff
 DBHOST = os.environ.get('DBHOST')
 DBUSER = os.environ.get('DBUSER')
